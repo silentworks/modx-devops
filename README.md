33b9e57f6e0477a7b20245d1b540b0e7c4934fde
Andrew Smith <a.smith@silentworks.co.uk>
2013-08-17 14:23:24 +0100

    MODX setup on my devops base

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# Vagrant setup for Apache, PHP, MySQL, xdebug
+# Vagrant setup for MODX with Apache, PHP, MySQL, xdebug
 
 You can install this repository by running the following commands:
 
 ```bash
-git clone git@bitbucket.org:silentworks/devops.git
-cd devops
+git clone https://github.com/silentworks/modx-devops.git
+cd modx-devops
 vagrant up
 ```
+
+Once Vagrant provisioning is successful, you can then navigate to http://192.168.33.121/setup in your web browser.
+
+### Database Info
+
+- username: vagrant
+- password: vagrant
+- database name: modx
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,1 +1,49 @@
-class { "devops": }
+$mysql_host = 'localhost'
+$mysql_db   = 'modx'
+$mysql_user = 'vagrant'
+$mysql_pass = 'vagrant'
+
+class { "devops":
+    mysql_host => $mysql_host,
+    mysql_db => $mysql_db,
+    mysql_user => $mysql_user,
+    mysql_pass => $mysql_pass,
+}
+
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+exec { 'modx_git':
+    cwd    => '/vagrant',
+    command => 'git clone https://github.com/rthrash/revolution.git www',
+    onlyif  => 'test ! -d /vagrant/www',
+    require => Package['git']
+}
+
+file { '/vagrant/www/_build/build.distrib.config.php':
+    content => template('devops/modx/build.distrib.config.sample.php.erb'),
+    ensure  => present,
+    require => Exec['modx_git'],
+}
+
+file { '/vagrant/www/_build/build.config.php':
+    content => template('devops/modx/build.config.sample.php.erb'),
+    ensure  => present,
+    require => Exec['modx_git'],
+}
+
+file { '/vagrant/www/_build/build.properties.php':
+    content => template('devops/modx/build.properties.sample.php.erb'),
+    ensure  => present,
+    require => Exec['modx_git'],
+}
+
+exec { 'build_transport':
+    cwd    => '/vagrant/www/_build',
+    path   => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin',
+    command => 'php /vagrant/www/_build/transport.core.php',
+    onlyif  => 'test -d /vagrant/www/_build',
+    require => [ Class['devops'], Class['php'], Exec['modx_git'], 
+                File['/vagrant/www/_build/build.distrib.config.php'], 
+                File['/vagrant/www/_build/build.config.php'],
+                File['/vagrant/www/_build/build.properties.php'] ],
+}
